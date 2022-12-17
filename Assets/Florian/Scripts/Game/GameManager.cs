61099cacb64bec45428e46713d551b15938c9da8
Krtz_Dev <florian.krtz@gmail.com>
2022-12-17 18:00:55 +0100

    Win after Time / Win after killing "Boss"

--- Assets/Florian/Scripts/Game/GameManager.cs
+++ Assets/Florian/Scripts/Game/GameManager.cs
@@ -6,10 +6,25 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager instance { get; private set; }
 
+    private enum WinningCondition
+    {
+        KillAllEnemies,
+        SurviveForTime,
+        KillSpecificEnemy
+    }
+
     [SerializeField]
     private GameObject _endscreen_Prefab;
+    [SerializeField]
+    private WinningCondition _winningCondition;
 
+    [Header("WinningCondition")]
+    [SerializeField]
+    private float _timeToSurvive;
+
+    private EnemySpawner _enemySpawner;
     private int _neededEnemyKill;
+    private bool _hasWon;
 
     private void Awake()
     {
@@ -21,20 +36,42 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
-        EnemySpawner enemySpawner = GameObject.Find("EnemySpawner").GetComponent<EnemySpawner>();
-        _neededEnemyKill = enemySpawner.EnemyWavesToSpawn * enemySpawner.EnemyWaveSize;
+        _enemySpawner = GameObject.Find("EnemySpawner").GetComponent<EnemySpawner>();
+        _neededEnemyKill = _enemySpawner.EnemyWavesToSpawn * _enemySpawner.EnemyWaveSize;
+    }
+
+    private void Update()
+    {
+        _timeToSurvive -= Time.deltaTime;
+        if (!_hasWon && _timeToSurvive <= 0 && _winningCondition == WinningCondition.SurviveForTime)
+        {
+            _hasWon = true;
+            RoundWon();
+        }
     }
 
     public void EnemyDied()
     {
         _neededEnemyKill--;
-        if (_neededEnemyKill == 0)
+        if (!_hasWon && _neededEnemyKill == 0 && _winningCondition == WinningCondition.KillAllEnemies)
         {
+            _hasWon = true;
+            RoundWon();
+        }
+
+        if (!_hasWon && _neededEnemyKill == 0 && _winningCondition == WinningCondition.KillSpecificEnemy)
+        {
+            _enemySpawner.SpawnRandomEnemy();
+        }
+        if (!_hasWon && _neededEnemyKill == -1 && _winningCondition == WinningCondition.KillSpecificEnemy)
+        {
+
+            _hasWon = true;
             RoundWon();
         }
     }
 
-    public void RoundWon()
+    private void RoundWon()
     {
         _endscreen_Prefab.SetActive(true);
     }
