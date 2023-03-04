7f2a8ccc1d29e09bc8e0a060f1bbf9c71496d81d
Lucas P <LucasPoethe@web.de>
2023-03-05 00:06:31 +0100

    Gizmos stick to Player. Rotation doesn't match Player rotation.

--- Assets/Lucas/Scripts/Enemies/NEW_EnemySpawner.cs
+++ Assets/Lucas/Scripts/Enemies/NEW_EnemySpawner.cs
@@ -17,40 +17,74 @@ public class NEW_EnemySpawner : MonoBehaviour
 
     private void OnDrawGizmos()
     {
+        // Variablen
         float BoxHeight = 0.25f;
 
         float CloseZoneRadius = 8f;
         float MidZoneRadius = 16f;
         float FarZoneRadius = 24f;
 
+        Vector3 CenterVector;
+
+        // Settings speichern
+        Color prevColor = Gizmos.color;
+        Matrix4x4 prevMatrix = Gizmos.matrix;
+
         // Frontal
         Gizmos.color = Color.cyan;
         Vector3 FrontalVector = new Vector3(FarZoneRadius, BoxHeight, CloseZoneRadius * 2);
-        Gizmos.DrawCube(PlayerTransform.position + new Vector3(FarZoneRadius / 2, 0, 0), FrontalVector);
+        CenterVector = PlayerTransform.position + new Vector3(FarZoneRadius / 2, 0, 0);
+        CenterVector = transform.InverseTransformPoint(CenterVector);
+        Gizmos.DrawCube(CenterVector, FrontalVector);
+
         // Rear
         Gizmos.color = Color.magenta;
         Vector3 RearVector = new Vector3(FarZoneRadius, BoxHeight, CloseZoneRadius * 2);
-        Gizmos.DrawCube(PlayerTransform.position - new Vector3(FarZoneRadius / 2, 0, 0), RearVector);
+        CenterVector = PlayerTransform.position - new Vector3(FarZoneRadius / 2, 0, 0);
+        CenterVector = transform.InverseTransformPoint(CenterVector);
+        Gizmos.DrawCube(CenterVector, RearVector);
+
         // Left Lateral
         Gizmos.color = Color.green;
         Vector3 LeftLateralVector = new Vector3(CloseZoneRadius * 2, BoxHeight, CloseZoneRadius * 2);
-        Gizmos.DrawCube(PlayerTransform.position + new Vector3(0, 0, CloseZoneRadius * 2), LeftLateralVector);
+        CenterVector = PlayerTransform.position + new Vector3(0, 0, CloseZoneRadius * 2);
+        CenterVector = transform.InverseTransformPoint(CenterVector);
+        Gizmos.DrawCube(CenterVector, LeftLateralVector);
+
         // Right Lateral
         Vector3 RightLateralVector = new Vector3(CloseZoneRadius * 2, BoxHeight, CloseZoneRadius * 2);
-        Gizmos.DrawCube(PlayerTransform.position - new Vector3(0, 0, CloseZoneRadius * 2), RightLateralVector);
+        CenterVector = PlayerTransform.position - new Vector3(0, 0, CloseZoneRadius * 2);
+        CenterVector = transform.InverseTransformPoint(CenterVector);
+        Gizmos.DrawCube(CenterVector, RightLateralVector);
+
         // Left Up Peripheral
         Gizmos.color = Color.yellow;
         Vector3 LeftUpPeripheralVector = new Vector3(CloseZoneRadius * 2, BoxHeight, CloseZoneRadius * 2);
-        Gizmos.DrawCube(PlayerTransform.position + new Vector3(CloseZoneRadius * 2, 0, CloseZoneRadius * 2), LeftUpPeripheralVector);
+        CenterVector = PlayerTransform.position + new Vector3(CloseZoneRadius * 2, 0, CloseZoneRadius * 2);
+        CenterVector = transform.InverseTransformPoint(CenterVector);
+        Gizmos.DrawCube(CenterVector, LeftUpPeripheralVector);
+
         // Right Up Peripheral
         Vector3 RightUpPeripheralVector = new Vector3(CloseZoneRadius * 2, BoxHeight, CloseZoneRadius * 2);
-        Gizmos.DrawCube(PlayerTransform.position + new Vector3(-CloseZoneRadius * 2, 0, CloseZoneRadius * 2), RightUpPeripheralVector);
+        CenterVector = PlayerTransform.position + new Vector3(-CloseZoneRadius * 2, 0, CloseZoneRadius * 2);
+        CenterVector = transform.InverseTransformPoint(CenterVector);
+        Gizmos.DrawCube(CenterVector, RightUpPeripheralVector);
+
         // Left Down Peripheral
         Vector3 LeftDownPeripheralVector = new Vector3(CloseZoneRadius * 2, BoxHeight, CloseZoneRadius * 2);
-        Gizmos.DrawCube(PlayerTransform.position + new Vector3(CloseZoneRadius * 2, 0, -CloseZoneRadius * 2), LeftDownPeripheralVector);
+        CenterVector = PlayerTransform.position + new Vector3(CloseZoneRadius * 2, 0, -CloseZoneRadius * 2);
+        CenterVector = transform.InverseTransformPoint(CenterVector);
+        Gizmos.DrawCube(CenterVector, LeftDownPeripheralVector);
+
         // Right Down Peripheral
         Vector3 RightDownPeripheralVector = new Vector3(CloseZoneRadius * 2, BoxHeight, CloseZoneRadius * 2);
-        Gizmos.DrawCube(PlayerTransform.position + new Vector3(-CloseZoneRadius * 2, 0, -CloseZoneRadius * 2), RightDownPeripheralVector);
+        CenterVector = PlayerTransform.position + new Vector3(-CloseZoneRadius * 2, 0, -CloseZoneRadius * 2);
+        CenterVector = transform.InverseTransformPoint(CenterVector);
+        Gizmos.DrawCube(CenterVector, RightDownPeripheralVector);
+
+        // Settings resetten
+        Gizmos.color = prevColor;
+        Gizmos.matrix = prevMatrix;
     }
 
 }
