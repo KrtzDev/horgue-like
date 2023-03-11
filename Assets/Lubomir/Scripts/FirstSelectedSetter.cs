375966bf3b4a7fcee1a0cdc1c77aa5835638dec5
Krtz_Dev <florian.krtz@gmail.com>
2023-03-11 17:40:49 +0100

    new crazy WeaponParts, Fixed Level/Wave Number after death, implemented crit

--- Assets/Lubomir/Scripts/FirstSelectedSetter.cs
+++ Assets/Lubomir/Scripts/FirstSelectedSetter.cs
@@ -3,22 +3,12 @@ using UnityEngine.EventSystems;
 
 public class FirstSelectedSetter : MonoBehaviour
 {
-
-	private void OnEnable()
+	private void Start()
 	{
-		if(SceneLoader.Instance)
-			SceneLoader.Instance.CompletedSceneLoad += OnSceneLoaded;
-	}
-
-	private void OnDisable()
-	{
-		if(SceneLoader.Instance)
-		SceneLoader.Instance.CompletedSceneLoad -= OnSceneLoaded;
-	}
-
-	private void OnSceneLoaded()
-	{
-		EventSystem.current.firstSelectedGameObject = gameObject;
-		EventSystem.current.SetSelectedGameObject(gameObject);
+		if (EventSystem.current)
+		{
+			EventSystem.current.firstSelectedGameObject = gameObject;
+			EventSystem.current.SetSelectedGameObject(gameObject);
+		}
 	}
 }
