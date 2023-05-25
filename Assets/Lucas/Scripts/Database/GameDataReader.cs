ff6e2a507c60b4bd3a6e50befb43069015747560
Lucas P <LucasPoethe@web.de>
2023-05-25 22:37:01 +0200

    Added Database Update to the Live Game (Main Menu).
    Database will be updated on Main Menu and Start will only be able if Data was loaded.
    If there is no Internet there should a short delay and then the Data will be loaded from the last JSON file.
    
    We still get an Error:
    Error: a native collection has not been disposed resulting in a memory leak

--- Assets/Lucas/Scripts/Database/GameDataReader.cs
+++ Assets/Lucas/Scripts/Database/GameDataReader.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameDataReader : MonoBehaviour
 {
     public TextAsset gameData;
+    public Button startButton;
 
     [Header("PLAYER DATA")]
     [SerializeField]
@@ -41,21 +44,21 @@ public class GameDataReader : MonoBehaviour
     {
         public string Name;
         public float movementSpeed;
-        public int maxHealth;
+        public float maxHealth;
     }
 
     [System.Serializable]
     public class Enemy
     {
         public string Name;
-        public int maxHealth;
+        public float maxHealth;
         public float damagePerHit;
         public float attackSpeed;
-        public int givenXP;
+        public float givenXP;
         public float moveSpeed;
-        public int armor;
-        public int elementalResistance;
-        public int technicalResistance;
+        public float armor;
+        public float elementalResistance;
+        public float technicalResistance;
     }
 
     [System.Serializable]
@@ -66,7 +69,7 @@ public class GameDataReader : MonoBehaviour
         public float damageBonus;
         public int WavesToSpawn;
         public int WaveSize;
-        public int SpawnDelay;
+        public float SpawnDelay;
         public int TimeToSurvive;
     }
 
@@ -92,65 +95,98 @@ public class GameDataReader : MonoBehaviour
     public EnemyList myEnemyList = new();
     public LevelList myLevelList = new();
 
-    private void Start()
+    private void OnEnable()
+    {
+        StartCoroutine(GetListsFromJSON());
+    }
+
+    private IEnumerator GetListsFromJSON()
     {
         myPlayerList = JsonUtility.FromJson<PlayerList>(gameData.text);
         myEnemyList = JsonUtility.FromJson<EnemyList>(gameData.text);
         myLevelList = JsonUtility.FromJson<LevelList>(gameData.text);
-        GetPlayerData();
-        GetEnemyData();
-        GetLevelData();
+
+        yield return null;
+
+        StartCoroutine(GetData());
     }
 
-    private void GetPlayerData()
+    private IEnumerator GetData()
+    {
+        StartCoroutine(GetPlayerData());
+        StartCoroutine(GetEnemyData());
+        StartCoroutine(GetLevelData());
+
+        yield return null;
+
+        Debug.Log("DONE DATA");
+
+        StartCoroutine(EnableButton());
+    }
+
+    private IEnumerator EnableButton()
+    {
+        startButton.GetComponent<Button>().interactable = true;
+        startButton.GetComponentInChildren<TextMeshProUGUI>().text = "START";
+
+        yield return null;
+
+        Debug.Log("DONE BUTTON");
+    }
+
+    private IEnumerator GetPlayerData()
     {
         _playerData._movementSpeed = myPlayerList.Player[0].movementSpeed;
-        _playerData._maxHealth = myPlayerList.Player[0].maxHealth;
+        _playerData._maxHealth = Mathf.RoundToInt(myPlayerList.Player[0].maxHealth);
+
+        yield return null;
     }
 
-    private void GetEnemyData()
+    private IEnumerator GetEnemyData()
     {
-        _basicEnemyData._maxHealth = myEnemyList.Enemies[0].maxHealth; //* GameManager.Instance._GameManagerValues[GameManager.Instance._currentLevelArray]._healthBonus);
+        _basicEnemyData._maxHealth = (int)myEnemyList.Enemies[0].maxHealth; //* GameManager.Instance._GameManagerValues[GameManager.Instance._currentLevelArray]._healthBonus);
         _basicEnemyData._damagePerHit = (int)myEnemyList.Enemies[0].damagePerHit; //* GameManager.Instance._GameManagerValues[GameManager.Instance._currentLevelArray]._damageBonus);
         _basicEnemyData._attackSpeed = myEnemyList.Enemies[0].attackSpeed;
-        _basicEnemyData._givenXP = myEnemyList.Enemies[0].givenXP;
+        _basicEnemyData._givenXP = (int)myEnemyList.Enemies[0].givenXP;
         _basicEnemyData._moveSpeed = myEnemyList.Enemies[0].moveSpeed;
         _basicEnemyData._armor = myEnemyList.Enemies[0].armor;
         _basicEnemyData._elementalResistance = myEnemyList.Enemies[0].elementalResistance;
         _basicEnemyData._technicalResistance = myEnemyList.Enemies[0].technicalResistance;
         Debug.Log("Basic");
 
-        _rangedRobotData._maxHealth = (int)(myEnemyList.Enemies[1].maxHealth * _basicEnemyData._maxHealth);
-        _rangedRobotData._damagePerHit = (int)(myEnemyList.Enemies[1].damagePerHit * _basicEnemyData._damagePerHit);
+        _rangedRobotData._maxHealth = Mathf.RoundToInt((myEnemyList.Enemies[1].maxHealth * myEnemyList.Enemies[0].maxHealth));
+        _rangedRobotData._damagePerHit = Mathf.RoundToInt(myEnemyList.Enemies[1].damagePerHit * myEnemyList.Enemies[0].damagePerHit);
         _rangedRobotData._attackSpeed = myEnemyList.Enemies[1].attackSpeed;
-        _rangedRobotData._givenXP = myEnemyList.Enemies[1].givenXP;
+        _rangedRobotData._givenXP = (int)myEnemyList.Enemies[1].givenXP;
         _rangedRobotData._moveSpeed = myEnemyList.Enemies[1].moveSpeed;
         _rangedRobotData._armor = myEnemyList.Enemies[1].armor;
         _rangedRobotData._elementalResistance = myEnemyList.Enemies[1].elementalResistance;
         _rangedRobotData._technicalResistance = myEnemyList.Enemies[1].technicalResistance;
         Debug.Log("RangedRobot");
 
-        _pasuKanData._maxHealth = (int)(myEnemyList.Enemies[2].maxHealth * _basicEnemyData._maxHealth);
-        _pasuKanData._damagePerHit = (int)(myEnemyList.Enemies[2].damagePerHit * _basicEnemyData._damagePerHit);
+        _pasuKanData._maxHealth = Mathf.RoundToInt(myEnemyList.Enemies[2].maxHealth * myEnemyList.Enemies[0].maxHealth);
+        _pasuKanData._damagePerHit = Mathf.RoundToInt(myEnemyList.Enemies[2].damagePerHit * myEnemyList.Enemies[0].damagePerHit);
         _pasuKanData._attackSpeed = myEnemyList.Enemies[2].attackSpeed;
-        _pasuKanData._givenXP = myEnemyList.Enemies[2].givenXP;
+        _pasuKanData._givenXP = (int)myEnemyList.Enemies[2].givenXP;
         _pasuKanData._moveSpeed = myEnemyList.Enemies[2].moveSpeed;
         _pasuKanData._armor = myEnemyList.Enemies[2].armor;
         _pasuKanData._elementalResistance = myEnemyList.Enemies[2].elementalResistance;
         _pasuKanData._technicalResistance = myEnemyList.Enemies[2].technicalResistance;
         Debug.Log("PasuKan");
 
-        _gigaRangedRobotData._maxHealth = (int)(myEnemyList.Enemies[3].maxHealth * _basicEnemyData._maxHealth);
-        _gigaRangedRobotData._damagePerHit = (int)(myEnemyList.Enemies[3].damagePerHit * _basicEnemyData._damagePerHit);
+        _gigaRangedRobotData._maxHealth = Mathf.RoundToInt(myEnemyList.Enemies[3].maxHealth * myEnemyList.Enemies[0].maxHealth);
+        _gigaRangedRobotData._damagePerHit = Mathf.RoundToInt(myEnemyList.Enemies[3].damagePerHit * myEnemyList.Enemies[0].damagePerHit);
         _gigaRangedRobotData._attackSpeed = myEnemyList.Enemies[3].attackSpeed;
-        _gigaRangedRobotData._givenXP = myEnemyList.Enemies[3].givenXP;
+        _gigaRangedRobotData._givenXP = (int)myEnemyList.Enemies[3].givenXP;
         _gigaRangedRobotData._moveSpeed = myEnemyList.Enemies[3].moveSpeed;
         _gigaRangedRobotData._armor = myEnemyList.Enemies[3].armor;
         _gigaRangedRobotData._elementalResistance = myEnemyList.Enemies[3].elementalResistance;
         _gigaRangedRobotData._technicalResistance = myEnemyList.Enemies[3].technicalResistance;
         Debug.Log("GIGA RangedRobot");
+
+        yield return null;
     }
-    private void GetLevelData()
+    private IEnumerator GetLevelData()
     {
         _Level01._healthBonus = myLevelList.Level[0].healthBonus;
         _Level01._damageBonus = myLevelList.Level[0].damageBonus;
@@ -173,5 +209,6 @@ public class GameDataReader : MonoBehaviour
         _enemySpawnerData3._enemyWaveSize = myLevelList.Level[2].WaveSize;
         _enemySpawnerData3._enemySpawnDelay = myLevelList.Level[2].SpawnDelay;
 
+        yield return null;
     }
 }
--- Assets/Lucas/Scripts/Database/UpdateTuningData.cs
+++ Assets/Lucas/Scripts/Database/UpdateTuningData.cs
@@ -13,6 +13,11 @@ using UnityEngine.Networking;
 
 public class UpdateTuningData : MonoBehaviour
 {
+    public void Start()
+    {
+		GetTuningDataGame();
+    }
+
     public class ExportError
 	{
 		public string status;
@@ -25,20 +30,21 @@ public class UpdateTuningData : MonoBehaviour
 	private static List<string[]> s_DataUpdateQueue = new List<string[]>();
 	static UnityWebRequest www;
 
-	[MenuItem("GetJSON/Get Game Data")]
-	public static void GetTuningData()
-	{
+	public GameObject GameDataReader;
+	public bool dataRetrieved = false;
 
+	public void GetTuningDataGame()
+	{
 		s_DataUpdateQueue.Clear();
 		// ADD ALL SPREADSHEETS YOU WANT DO RETRIEVE CONFIG DATA FROM
 		// FIRST PART IS LOCAL FILE NAME TO CACHE (IN ASSETS FOLDER), SECOND IS SPREADSHEET KEY
 		s_DataUpdateQueue.Add(new string[] { "GameData.json", "1dbgvJsZAh6RdSJZYxfwGvvmaSeoRMNVGeIIugai8UIU" });
 
-		SendNextRequest();
-		if (www != null) EditorApplication.update += EditorUpdate;
+		Debug.Log("Get Data");
+		SendNextRequestGame();
 	}
 
-	static void SendNextRequest()
+	static void SendNextRequestGame()
 	{
 		if (s_DataUpdateQueue.Count == 0)
 		{
@@ -49,7 +55,105 @@ public class UpdateTuningData : MonoBehaviour
 		WWWForm form = new WWWForm();
 		form.AddField("key", s_DataUpdateQueue[0][1]); // Add spreadsheet key to post darta			
 		www = UnityWebRequest.Post(ExportSheetScriptURL, form);
-		www.Send();
+		www.SendWebRequest();
+	}
+
+	private void Update()
+	{
+		if (!dataRetrieved)
+		{
+			while (!www.isDone)
+				return;
+
+			bool error = false;
+			string errorMessage = "";
+			// General error with transport?
+			if (www.result == UnityWebRequest.Result.ConnectionError)
+			{
+				error = true;
+				errorMessage = "Error updating data from sheet '" + s_DataUpdateQueue[0][1] + "' : " + www.error;
+
+				dataRetrieved = true;
+				GameDataReader.SetActive(true);
+			}
+			else
+			{
+				// Detect error with data
+				try
+				{
+					ExportError jsonError = JsonUtility.FromJson<ExportError>(www.downloadHandler.text);
+					if (jsonError != null && jsonError.error != null)
+					{
+						error = true;
+						errorMessage = "Error updating data from sheet '" + jsonError.error + "'";
+
+						dataRetrieved = true;
+						GameDataReader.SetActive(true);
+					}
+				}
+				catch
+				{
+					// This is all good, means data is not an error packet 
+				}
+			}
+
+			if (error)
+			{
+				Debug.LogError(errorMessage);
+				EditorApplication.update -= Update;
+
+				dataRetrieved = true;
+				GameDataReader.SetActive(true);
+			}
+			else
+			{
+				Debug.Log("Data successfully received from sheet '" + s_DataUpdateQueue[0][1] + "'");
+				string writePath = "Assets/" + s_DataUpdateQueue[0][0];
+				System.IO.File.WriteAllText(writePath, www.downloadHandler.text);
+				s_DataUpdateQueue.RemoveAt(0);
+				if (s_DataUpdateQueue.Count == 0)
+				{
+					// Queue complete
+					dataRetrieved = true;
+					GameDataReader.SetActive(true);
+					EditorApplication.update -= Update;
+					AssetDatabase.Refresh();
+					www = null;
+				}
+				else
+				{
+					SendNextRequestGame();
+				}
+			}
+		}
+	}
+
+	/*
+
+	[MenuItem("GetJSON/Get Game Data")]
+	public static void GetTuningDataEditor()
+	{
+		s_DataUpdateQueue.Clear();
+		// ADD ALL SPREADSHEETS YOU WANT DO RETRIEVE CONFIG DATA FROM
+		// FIRST PART IS LOCAL FILE NAME TO CACHE (IN ASSETS FOLDER), SECOND IS SPREADSHEET KEY
+		s_DataUpdateQueue.Add(new string[] { "GameData.json", "1dbgvJsZAh6RdSJZYxfwGvvmaSeoRMNVGeIIugai8UIU" });
+
+		SendNextRequestEditor();
+		if (www != null) EditorApplication.update += EditorUpdate;
+	}
+
+    static void SendNextRequestEditor()
+	{
+		if (s_DataUpdateQueue.Count == 0)
+		{
+			Debug.LogError("Queue empty - add some items!");
+			return;
+		}
+		Debug.Log("Retrieving '" + s_DataUpdateQueue[0][0] + "' from sheet '" + s_DataUpdateQueue[0][1] + "'");
+		WWWForm form = new WWWForm();
+		form.AddField("key", s_DataUpdateQueue[0][1]); // Add spreadsheet key to post darta			
+		www = UnityWebRequest.Post(ExportSheetScriptURL, form);
+		www.SendWebRequest();
 	}
 
 	static void EditorUpdate()
@@ -60,7 +164,7 @@ public class UpdateTuningData : MonoBehaviour
 		bool error = false;
 		string errorMessage = "";
 		// General error with transport?
-		if (www.isNetworkError)
+		if (www.result == UnityWebRequest.Result.ConnectionError)
 		{
 			error = true;
 			errorMessage = "Error updating data from sheet '" + s_DataUpdateQueue[0][1] + "' : " + www.error;
@@ -103,8 +207,10 @@ public class UpdateTuningData : MonoBehaviour
 			}
 			else
 			{
-				SendNextRequest();
+				SendNextRequestEditor();
 			}
 		}
 	}
+
+	*/
 }
