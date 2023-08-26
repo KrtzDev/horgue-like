1b00526291f4710f301e79d4bc524e8110d547b8
Lucas <LucasPoethe@web.de>
2023-08-26 15:29:24 +0200

    Character Mapping Implementation (- SwitchMode).

--- Assets/Scripts/PlayerCharacter/CameraControls.cs
+++ Assets/Scripts/PlayerCharacter/CameraControls.cs
@@ -1,4 +1,5 @@
 using Cinemachine;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,15 +12,25 @@ public class CameraControls : MonoBehaviour
     private CinemachineVirtualCamera _cm;
     private CinemachineOrbitalTransposer _tr;
 
+    private PlayerInputMappings _inputActions;
+    private float _moveDir;
+
     private void Start()
     {
         _cm = GetComponent<CinemachineVirtualCamera>();
         _tr = _cm.GetCinemachineComponent<CinemachineOrbitalTransposer>();
-    }
 
+        _inputActions = new PlayerInputMappings();
+
+        _inputActions.Character.Enable();
+
+        _inputActions.Character.CameraMovement.performed += MoveCamera;
+        _inputActions.Character.CameraMovement.canceled += ReleasedButton;
+    }
 
     private void Update()
     {
+        /*
         var keyboard = Keyboard.current;
 
         if (keyboard.qKey.wasPressedThisFrame)
@@ -33,6 +44,32 @@ public class CameraControls : MonoBehaviour
         else if (keyboard.rKey.wasPressedThisFrame)
         {
             _tr.m_XAxis.Value = 0.0f;
+        }
+        */
+
+        if(_moveDir < 0)
+        {
+            _tr.m_XAxis.Value -= Time.deltaTime * _rotation;
+        }
+        else if (_moveDir > 0)
+        {
+            _tr.m_XAxis.Value += Time.deltaTime * _rotation;
+        }
+    }
+
+    private void MoveCamera(InputAction.CallbackContext ctx)
+    {
+        if(ctx.performed)
+        {
+            _moveDir = ctx.ReadValue<float>();
+        }
+    }
+
+    private void ReleasedButton(InputAction.CallbackContext ctx)
+    {
+        if(ctx.canceled)
+        {
+            _moveDir = 0;
         }
     }
 }
