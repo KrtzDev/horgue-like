e5ec49299ea0e9b4276f3112ae325692b529cc5e
Lucas <LucasPoethe@web.de>
2023-09-14 00:08:08 +0200

    Playtest Credits.

--- Assets/Scripts/UI/Credits.cs
+++ Assets/Scripts/UI/Credits.cs
@@ -2,12 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Credits : MonoBehaviour
 {
+    [SerializeField] private bool _mainCredits;
     [SerializeField] private TMP_Text _firstName;
     [SerializeField] private TMP_Text _lastName;
     [SerializeField] private TMP_Text _role;
+
+
+    [SerializeField] private GameObject _creditsPlaytest;
+    [SerializeField] private Image _background;
+    [SerializeField] private MainMenu _mainMenu;
+    [SerializeField] private float _timeToFade;
+
+    private void Awake()
+    {
+        if(_mainCredits)
+            SetCreditsPlayTestFalse();
+    }
 
     public void ChangeFirstName(string name)
     {
@@ -29,6 +43,43 @@ public class Credits : MonoBehaviour
 
     public void StartPlayTestCredits()
     {
+        _creditsPlaytest.SetActive(true);
+    }
 
+    private void StartFadeToBlack()
+    {
+        StartCoroutine(FadeToBlack());
+    }
+
+    private IEnumerator FadeToBlack()
+    {
+        _mainMenu.DeactivateCreditsButton();
+
+        float elapsedTime = 0;
+
+        while(elapsedTime < _timeToFade)
+        {
+            elapsedTime += Time.unscaledDeltaTime;
+
+            Color lerpedColor = Color.Lerp(_background.color, Color.black, (elapsedTime / _timeToFade));
+
+            _background.color = lerpedColor;
+
+            yield return null;
+        }
+
+        ReturnToMainMenu();
+    }
+
+    public void ReturnToMainMenu()
+    {
+        SetCreditsPlayTestFalse();
+        _mainMenu.StartCredits();
+        _background.color = new Color(0, 0, 0, 0);
+    }
+
+    public void SetCreditsPlayTestFalse()
+    {
+        _creditsPlaytest.SetActive(false);
     }
 }
